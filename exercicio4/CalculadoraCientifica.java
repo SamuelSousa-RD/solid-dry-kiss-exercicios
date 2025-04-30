587be86fe452fdc24074fb2156d5d0e0f977cb35
SamuelSousa-RD <samuelfilho.rd@gmail.com>
2025-04-29 22:27:25 -0300

    Create CalculadoraCientifica.java

---
+++ exercicio4/CalculadoraCientifica.java
@@ -0,0 +1,8 @@
+public class CalculadoraCientifica extends Calculadora {
+    public double raizQuadrada(double a) { 
+      return Math.sqrt(a); 
+    }
+    public double potencia(double base, double expoente) {
+      return Math.pow(base, expoente); 
+    }
+}
